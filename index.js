6c8e99d4a6db75c85941f6777a9410f31d94275e
Steven Brock <shyfay0@gmail.com>
2021-08-30 09:37:51 -0700

    commit tree 52e211b524c91b155dc8f7c3fe278bc1389af205

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+const companyName = "Scuber"
+    //   1) is set as Scuber
+    //   2) is defined as a const
+    // mostProfitableNeighborhood
+    //   3) is declared as equal to Chelsea
+    //   4) is defined using let
+    // companyCeo
+    //   5) is declared as equal to Susan Smith
+    //   6) is defined using let
+
+      let mostProfitableNeighborhood = "Chelsea"
+      let companyCeo = "Susan Smith"
